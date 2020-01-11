9b7b1833e42d962d608793e24e5b35471595d9a4
DavidFFDAW <davidferflo2@gmail.com>
2020-01-11 21:57:35 +0100

    Ejercicio 1 de Repaso añadido de nuevo

---
+++ ProgramacionOO/Ejercicio1DeRepaso/src/App.java
@@ -0,0 +1,10 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        InterfazMenu menuPrograma = new InterfazMenu();
+        menuPrograma.mostrarMenuUnidades();
+    }
+}
