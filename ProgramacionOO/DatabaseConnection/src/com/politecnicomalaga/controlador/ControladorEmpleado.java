a6952cc640b2595018af392b06f0ac1f16a16066
DavidFFDAW <davidferflo2@gmail.com>
2020-03-12 17:34:48 +0100

    Añadido ejercicio de conexion a Base de Datos

---
+++ ProgramacionOO/DatabaseConnection/src/com/politecnicomalaga/controlador/ControladorEmpleado.java
@@ -0,0 +1,94 @@
+package com.politecnicomalaga.controlador;
+
+import com.politecnicomalaga.dao.EmpleadoDAO;
+import com.politecnicomalaga.modelo.Empleado;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class ControladorEmpleado {
+
+    public ArrayList<Empleado> getEmpleados(){
+        ArrayList<Empleado> listaEmpleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            listaEmpleados =  empleadoDAO.getEmpleados();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listaEmpleados;
+    }
+
+    public ArrayList<Empleado> getBusquedaPatron(String patron){
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            empleados = empleadoDAO.buscarPorPatron(patron);
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+
+    public ArrayList<Empleado> getListaTrasBorrado(String nombreMod){
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            empleados = empleadoDAO.borrar(nombreMod);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return  empleados;
+    }
+
+    public ArrayList<Empleado> createInserction(int id, String nombre, int edad){
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            empleados = empleadoDAO.inserction(id, nombre, edad);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+
+    public String getBusqueda(String name){
+        String nombre = "";
+        try{
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            nombre = empleadoDAO.buscar(name);
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return nombre;
+    }
+
+    public ArrayList<Empleado> mostrarMenoresDeEdad(){
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            empleados = empleadoDAO.busquedaMenoresEdad();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+
+    public ArrayList<Empleado> mostrarMayoresDeEdad(){
+        ArrayList<Empleado> empleados = new ArrayList<>();
+        try {
+            EmpleadoDAO empleadoDAO = new EmpleadoDAO();
+            empleados = empleadoDAO.busquedaMayoresEdad();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        return empleados;
+    }
+}
---
+++ ProgramacionOO/DatabaseConnection/src/com/politecnicomalaga/vista/VistaEmpleados.java
@@ -0,0 +1,116 @@
+package com.politecnicomalaga.vista;
+
+import com.politecnicomalaga.controlador.ControladorEmpleado;
+import com.politecnicomalaga.modelo.Empleado;
+import com.politecnicomalaga.vista.dialogos.CreateDialog;
+import com.politecnicomalaga.vista.dialogos.DeleteDialog;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+public class VistaEmpleados {
+    private JTextField txtPatron;
+    private JButton btnBuscar;
+    private JTable tblEmpleados;
+    private JPanel pnlEmpleado;
+    private JLabel lblPatron;
+    private JButton btnInsertar;
+    private JButton btnBorrar;
+    private JButton btnUpdate;
+    private JButton btnMayores;
+    private JButton btnMenores;
+    private ControladorEmpleado controlador;
+
+    public VistaEmpleados(ControladorEmpleado controlador){
+        this.controlador = controlador;
+        actualizarTabla(controlador.getEmpleados());
+        //btnMenores.setBackground(Color.decode("#ffff99"));
+
+        btnBuscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String patron = txtPatron.getText();
+                ArrayList<Empleado> lista = controlador.getBusquedaPatron(patron);
+                actualizarTabla(lista);
+            }
+        });
+
+        btnBorrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                DeleteDialog delete = new DeleteDialog();
+                delete.setLocationRelativeTo(SwingUtilities.getRoot((Component) actionEvent.getSource()));
+                delete.pack();
+                String nombreModificado = delete.getNombreBorrado();
+                int resp = JOptionPane.showConfirmDialog(SwingUtilities.getRoot((Component) actionEvent.getSource()),"¿ Estas seguro de que desea borrar: "+nombreModificado+" ?");
+                if (resp == JOptionPane.OK_OPTION){
+                    ArrayList<Empleado> listaEmpleados = controlador.getListaTrasBorrado(nombreModificado);
+                    actualizarTabla(listaEmpleados);
+                }
+            }
+        });
+
+        btnInsertar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                CreateDialog insert = new CreateDialog();
+                insert.setLocationRelativeTo(SwingUtilities.getRoot((Component) actionEvent.getSource()));
+                insert.pack();
+                int id = insert.getId();
+                String nombre = insert.getNombre();
+                int edad = insert.getEdad();
+                JOptionPane.showMessageDialog(SwingUtilities.getRoot((Component) actionEvent.getSource()),""+nombre+" será creado en breve...");
+                ArrayList<Empleado> empleados = controlador.createInserction(id, nombre, edad);
+                actualizarTabla(empleados);
+            }
+        });
+
+        /*btnUpdate.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                DeleteDialog update = new DeleteDialog();
+                update.setLocationRelativeTo(SwingUtilities.getRoot((Component) actionEvent.getSource()));
+                update.pack();
+                String nombrePaBuscar = update.getNombreBorrado();
+                txtPatron.setText(controlador.getBusqueda(nombrePaBuscar));
+            }
+        });*/
+        btnMenores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                actualizarTabla(controlador.mostrarMenoresDeEdad());
+            }
+        });
+        btnMayores.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                actualizarTabla(controlador.mostrarMayoresDeEdad());
+            }
+        });
+    }
+
+    public void actualizarTabla(ArrayList<Empleado> listaEmpleados){
+        DefaultTableModel model = (DefaultTableModel) tblEmpleados.getModel();
+        model.setRowCount(0);
+        for (Empleado empleado: listaEmpleados){
+            Object[] row = {empleado.getId(),empleado.getNombre(),empleado.getEdad()};
+            model.addRow(row);
+        }
+    }
+
+    private void createUIComponents() {
+        tblEmpleados = new JTable();
+        Object[] columnas = {"id","nombre","edad"};
+        DefaultTableModel tableModel = new DefaultTableModel();
+        tableModel.setColumnIdentifiers(columnas);
+        tblEmpleados.setModel(tableModel);
+    }
+
+    public JPanel getPanel(){
+        return pnlEmpleado;
+    }
+}
