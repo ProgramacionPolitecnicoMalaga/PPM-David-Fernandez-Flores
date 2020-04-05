f423c284ea588c130276201631df5806b0ed44d9
DavidFFDAW <davidferflo2@gmail.com>
2020-04-05 20:38:03 +0200

    Borrado de proyecto anterior

--- ProgramacionOO/AISLAMIENTO/ProyectoContraseñas/src/com/politecnico/algoritmo/ControlAlgoritmo.java
+++
@@ -1,9 +0,0 @@
-package com.politecnico.algoritmo;
-
-public class ControlAlgoritmo {
-    private String hash;
-
-    public void setHash(String hash){
-        this.hash = hash;
-    }
-}
--- ProgramacionOO/AISLAMIENTO/ProyectoContraseñas/src/com/politecnico/algoritmo/GeneradorAlgoritmo.java
+++
@@ -1,4 +0,0 @@
-package com.politecnico.algoritmo;
-
-public class GeneradorAlgoritmo {
-}
