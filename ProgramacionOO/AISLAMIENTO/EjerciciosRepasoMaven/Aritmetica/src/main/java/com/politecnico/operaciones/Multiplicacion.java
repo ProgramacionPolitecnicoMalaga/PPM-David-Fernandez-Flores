6245083283c129b1d73b220fa8c337a6c59692ab
DavidFFDAW <davidferflo2@gmail.com>
2020-05-13 00:46:31 +0200

    Aritmetica con tag añadido

---
+++ ProgramacionOO/AISLAMIENTO/EjerciciosRepasoMaven/Aritmetica/src/main/java/com/politecnico/operaciones/Multiplicacion.java
@@ -0,0 +1,16 @@
+package com.politecnico.operaciones;
+
+public class Multiplicacion {
+
+    public double multiplicar(double a, double b){
+        return a * b;
+    }
+
+    public int multiplicar(int a, int b){
+        return a * b;
+    }
+
+    public double multiplicar(double a, double b, double c){
+        return a * b * c;
+    }
+}
